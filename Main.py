ec92affb6ddbd5080e6fe56f0f0ecf8b5a6c86de
WhaleRed <redwhalecs@gmail.com>
2025-04-03 12:25:45 +0800

    GUI

---
+++ Main.py
@@ -0,0 +1,95 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class studentWindow(QMainWindow):
+  def __init__(self):
+    super(studentWindow, self).__init__()
+    loadUi("studentWindow.ui", self)
+
+    #Conneect Menu
+    self.viewPrograms.triggered.connect(self.programView)
+    self.viewColleges.triggered.connect(self.collegeView)
+
+    #Fix Table Ratio
+    self.studentTable.setColumnWidth(0,145)        
+    self.studentTable.setColumnWidth(1, 250)        
+    self.studentTable.setColumnWidth(2, 250)        
+
+  def programView(self):
+    widget.setWindowTitle("Program Window")               
+    widget.setCurrentIndex(widget.currentIndex()+1)
+  
+  def collegeView(self):
+    widget.setWindowTitle("College Window")               
+    widget.setCurrentIndex(widget.currentIndex()+2)
+
+
+class programWindow(QMainWindow):
+  def __init__(self):
+    super(programWindow, self).__init__()
+    loadUi("programWindow.ui", self)
+    
+    #Connect Menu
+    self.viewStudents.triggered.connect(self.studentView)
+    self.viewColleges.triggered.connect(self.collegeView)
+
+    #Fix Table Ratio       
+    self.programTable.setColumnWidth(0,145) 
+    self.programTable.setColumnWidth(1, 730)
+    self.programTable.setColumnWidth(2,145)         
+  
+  def studentView(self):
+    widget.setWindowTitle("Student Window")               
+    widget.setCurrentIndex(widget.currentIndex()-1)
+  
+  def collegeView(self):
+    widget.setWindowTitle("College Window") 
+    widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+class collegeWindow(QMainWindow):
+  def __init__(self):
+    super(collegeWindow, self).__init__()
+    loadUi("collegeWindow.ui", self)
+
+    #Connect Menu
+    self.viewStudents.triggered.connect(self.studentView)
+    self.viewPrograms.triggered.connect(self.programView)
+
+    #Fix Table Ratio       
+    self.collegeTable.setColumnWidth(0,145) 
+    self.collegeTable.setColumnWidth(1, 875)
+
+  def studentView(self):
+    widget.setWindowTitle("Student Window")               
+    widget.setCurrentIndex(widget.currentIndex()-2)
+  
+  def programView(self):
+    widget.setWindowTitle("Program Window") 
+    widget.setCurrentIndex(widget.currentIndex()-1)
+
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+
+  widget = QtWidgets.QStackedWidget()
+
+  studentwindow = studentWindow()
+  programwindow = programWindow()
+  collegewindow = collegeWindow()
+
+  widget.addWidget(studentwindow)
+  widget.addWidget(programwindow)
+  widget.addWidget(collegewindow)
+  widget.setFixedHeight(597)               
+  widget.setFixedWidth(1065)
+  widget.setWindowTitle("Student Window")               
+  widget.show()
+
+  try:
+    sys.exit(app.exec_())
+  except:
+    print("Exiting")
